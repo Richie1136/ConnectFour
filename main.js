23561e20d68f52ceb8ab4eede254d9f6c31411a6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-31 11:44:18 -0400

    Grabbing all of the squares, the result id and whos turn it is from inside the html file and grabbing it inside of the js file

---
+++ main.js
@@ -0,0 +1,5 @@
+const squares = document.querySelectorAll('.grid div')
+const result = document.querySelector('#result')
+const whosTurn = document.querySelector('#currentplayer')
+
+let currentPlayer = 1
