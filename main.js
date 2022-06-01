48a0c9e2b83e4796af19f1978897694f8c0b949c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-01 11:12:49 -0400

    Changing the current player

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const result = document.querySelector('#result')
 const whosTurn = document.querySelector('#currentplayer')
 
 let currentPlayer = 1
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].onclick = () => {
+    if (squares[i + 7].classList.contains('taken')) {
+      if (currentPlayer === 1) {
+        squares[i].classList.add('taken')
+        squares[i].classList.add('player-1')
+        currentPlayer = 2
+        whosTurn.innerHTML = currentPlayer
+      }
+    }
+  }
+}
